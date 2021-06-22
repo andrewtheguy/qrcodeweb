ba089d6d8cded2bc3c3c0325041b50014e2f8ced
Andrew Chen <andrew@jaanuu.com>
2021-06-22 00:31:02 -0700

    cleanup

--- src/GenerateQR.tsx
+++ src/GenerateQR.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { useState,useEffect } from 'react';
 import {Col, Form, FormControl, Nav, Navbar, NavDropdown, Row} from "react-bootstrap";
 import Container from "react-bootstrap/Container";
+import Canvg, {
+    presets, RenderingContext2D
+} from 'canvg';
 
-const QRCode = require('qrcode');
+const QRCode = require("qrcode-svg");
 
 function download(dataurl: string, filename: string) {
     var a = document.createElement("a");
@@ -22,23 +25,53 @@ const GenerateQR: React.FC = () => {
         setText(text);
     }
 
-
     const downloadPng = () =>{
-        if(text !==''){
-            QRCode.toDataURL(text, {type: 'image/png'}, function (err: Error, url: string) {
-                if (err) throw err;
-                download(url,'qr.png')
-            })
+        if(svg !==null) {
+
+                //memory leak, should clean up
+                let canvas = document.getElementById('draw-qr-save') as HTMLCanvasElement;
+                if (!canvas) {
+                    canvas = document.createElement('canvas');
+                    canvas.width=320;
+                    canvas.height=320;
+                    canvas.id='draw-qr-save';
+                }
+                const ctx = canvas.getContext('2d') as RenderingContext2D;
+
+
+                const DOMURL = window.URL || window.webkitURL || window;
+
+                const img = new Image();
+                const svgBlob = new Blob([svg], {type: 'image/svg+xml;charset=utf-8'});
+                const url = DOMURL.createObjectURL(svgBlob);
+
+                img.onload = function () {
+                    ctx.drawImage(img, 0, 0);
+                    DOMURL.revokeObjectURL(url);
+
+                    const imgURI = canvas
+                        .toDataURL('image/png');
+                    //alert(imgURI)
+                    download(imgURI, "qrcode.png")
+                };
+
+                img.src = url;
+
         }
     }
 
 
     useEffect(() => {
         if(text!==''){
-            QRCode.toString(text,{type:'svg'},function (err: Error, svgdata: string) {
-                if (err) throw err;
-                setSvg(svgdata);
-            });
+            const svgdata = new QRCode({
+                //width: 300,
+                //height: 300,
+                padding: 1,
+                content: text,
+                join: true,
+                container: "svg-viewbox" //Useful but not required
+            }).svg();
+            setSvg(svgdata);
         }else{
             setSvg(null);
         }
@@ -51,14 +84,14 @@ const GenerateQR: React.FC = () => {
                 <Col>
                     <div className="form-group">
                         <label htmlFor="qr_text">Enter Text: <button onClick={()=> setText('')}>Clear</button></label>
-                        <textarea className="form-control" id="qr_text" rows={3}  onChange={HandleChangeText}  value={text}></textarea>
+                        <textarea autoCorrect="off" autoCapitalize="none" className="form-control" id="qr_text" rows={3}  onChange={HandleChangeText}  value={text}></textarea>
                     </div>
                 </Col>
             </Row>
             <Row>
                 <Col>
                     <div>{typeof svg === 'string' ?
-                        <div style={{maxWidth: 320}}><div dangerouslySetInnerHTML={{ __html: svg!}}></div>
+                        <div style={{width:"100%",maxWidth: 320}}><div dangerouslySetInnerHTML={{ __html: svg!}}></div>
                             <button onClick={downloadPng}>Download PNG</button>
                         </div> :<span>No text entered</span>}</div>
                 </Col>
