6c0354354ecbbabbac4c468efca4531ed804c556
Andrew Chen <andrew@jaanuu.com>
2021-06-22 10:07:15 -0700

    fix nav

--- src/App.tsx
+++ src/App.tsx
@@ -38,18 +38,18 @@ const ExampleToast: React.FC = ({
 
 const App = () => (
     <Router>
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light">
             {/*<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>*/}
             {/*<Nav.Link href="#home">Home</Nav.Link>*/}
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
+            {/*<Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">*/}
                 <Nav className="mr-auto">
 
                     <NavLink exact
                              to="/"
                              className="navbar-brand"
                     >
-                        React-Bootstrap
+                        QR
                     </NavLink>
 
                     <NavLink exact
@@ -75,28 +75,7 @@ const App = () => (
                     >
                         About
                     </NavLink>
-
-
-                    {/*<Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
                 </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-success">Search</Button>
-                </Form>
-            </Navbar.Collapse>*/}
-                </Nav>
-            </Navbar.Collapse>
         </Navbar>
         <Container fluid>
             <Row>
